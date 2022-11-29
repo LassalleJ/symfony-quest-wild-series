bf256369f61fd13d76b8a7a495ac6c4f1fba1492
Jordan Lassalle <lassalle.jordan@gmail.com>
2022-11-29 17:34:15 +0100

    quest 16, form, with bonus

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -6,8 +6,10 @@ use App\Repository\CategoryRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: CategoryRepository::class)]
+#[Assert\EnableAutoMapping]
 class Category
 {
     #[ORM\Id]
@@ -16,6 +18,11 @@ class Category
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank]
+    #[Assert\Length(
+        max : 255,
+        maxMessage: 'La catégorie saisie {{ value }} est trop longue, elle ne devrait pas dépasser {{ limit }} caractères',
+    )]
     private ?string $name = null;
 
     #[ORM\OneToMany(mappedBy: 'category', targetEntity: Program::class)]
